09787ed16a8da3af07c09b5ec5793d79bd84d518
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-05 14:30:45 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -11,16 +11,34 @@ describe('Promise', function (){
                 } else {
                     done(false);
                 }
-            })
-        })
+            });
+        });
         it('should return false if the Promise contains not the exact value', function (done){
-            Promise.resolve(50).contains(50).then(x => {
+            Promise.resolve(50).contains(25).then(x => {
+                if(!x){
+                    done();
+                } else {
+                    done(false);
+                }
+            });
+        });
+        it('should return true if the Promise and the value passes the test function', function (done){
+            Promise.resolve(50).contains(50, (a, b) => a == b).then(x => {
                 if(x){
                     done();
                 } else {
                     done(false);
                 }
-            })
+            });
         })
-    })
-})
+        it('should return false if the Promise and the value fails the test function', function (done){
+            Promise.resolve(50).contains(25, (a, b) => a == b).then(x => {
+                if(!x){
+                    done();
+                } else {
+                    done(false);
+                }
+            });
+        });
+    });
+});
